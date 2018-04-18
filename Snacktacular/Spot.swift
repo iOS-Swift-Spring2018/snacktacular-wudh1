81789ce13a0e9d2a7222dfea73f03350c660935a
Dan Wu <wudh@bc.edu>
2018-04-18 00:18:40 -0400

    What's good

---
+++ Snacktacular/Spot.swift
@@ -0,0 +1,99 @@
+//
+//  Spot.swift
+//  Snacktacular
+//
+//  Created by Dan Wu on 4/15/18.
+//  Copyright © 2018 John Gallaugher. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+import Firebase
+import MapKit
+
+class Spot: NSObject, MKAnnotation {
+    var name: String
+    var address: String
+    var coordinate: CLLocationCoordinate2D
+    var averageRating: Double
+    var numberOfReviews: Int
+    var postingUserID: String
+    var documentID: String
+    
+    var longitude: CLLocationDegrees {
+        return coordinate.longitude
+    }
+    
+    var latitude: CLLocationDegrees {
+        return coordinate.latitude
+    }
+    
+    var title: String? {
+        return name
+    }
+    
+    var subtitle: String? {
+        return address
+    }
+    
+    var dictionary: [String: Any] {
+        return ["name": name, "address": address, "longitude": longitude, "latitude": latitude, "averageRating": averageRating, "numberOfReviews": numberOfReviews, "postingUserID": postingUserID]
+    }
+    
+    init(name: String, address: String, coordinate: CLLocationCoordinate2D, averageRating: Double, numberOfReviews: Int, postingUserID: String, documentID: String) {
+        self.name = name
+        self.address = address
+        self.coordinate = coordinate
+        self.averageRating = averageRating
+        self.numberOfReviews = numberOfReviews
+        self.postingUserID = postingUserID
+        self.documentID = documentID
+    }
+    
+    convenience override init() {
+        self.init(name: "", address: "", coordinate: CLLocationCoordinate2D(), averageRating: 0.0, numberOfReviews: 0, postingUserID: "", documentID: "")
+    }
+    
+    convenience init(dictionary: [String: Any]) {
+        let name = dictionary["name"] as! String? ?? ""
+        let address = dictionary["address"] as! String? ?? ""
+        let latitude = dictionary["latitude"] as! CLLocationDegrees? ?? 0.0
+        let longitude = dictionary["longitude"] as! CLLocationDegrees? ?? 0.0
+        let coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        let averageRating = dictionary["averageRating"] as! Double? ?? 0.0
+        let numberOfReviews = dictionary["numberOfReviews"] as! Int? ?? 0
+        let postingUserID = dictionary["postingUserID"] as! String? ?? ""
+        self.init(name: name, address: address, coordinate: coordinate, averageRating: averageRating, numberOfReviews: numberOfReviews, postingUserID: postingUserID, documentID: "")
+    }
+    
+    func saveData(completed: @escaping (Bool) -> ()) {
+        let db = Firestore.firestore()
+        guard let postingUserID = (Auth.auth().currentUser?.uid) else {
+            return completed(false)
+        }
+        self.postingUserID = postingUserID
+        let dataToSave = self.dictionary
+        if self.documentID != "" {
+            let ref = db.collection("spots").document(self.documentID)
+            ref.setData(dataToSave) { (error) in
+                if let error = error {
+                    completed(false)
+                }
+                else {
+                    completed(true)
+                }
+            }
+        }
+        else {
+            var ref: DocumentReference? = nil
+            ref = db.collection("spots").addDocument(data: dataToSave) { error in
+                if let error = error {
+                    completed(false)
+                }
+                else {
+                    completed(true)
+                }
+            }
+        }
+    }
+}
---
+++ Snacktacular/Spots.swift
@@ -0,0 +1,34 @@
+//
+//  Spots.swift
+//  Snacktacular
+//
+//  Created by Dan Wu on 4/15/18.
+//  Copyright © 2018 John Gallaugher. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+class Spots {
+    var spotArray = [Spot]()
+    var db: Firestore!
+    
+    init() {
+        db = Firestore.firestore()
+    }
+    
+    func loadData(completed: @escaping () -> ()) {
+        db.collection("spots").addSnapshotListener { (querySnapshot, error) in
+            guard error == nil else {
+                return completed()
+            }
+            self.spotArray = []
+            for document in querySnapshot!.documents {
+                let spot = Spot(dictionary: document.data())
+                spot.documentID = document.documentID
+                self.spotArray.append(spot)
+            }
+            completed()
+        }
+    }
+}
